cd0d6960f4bfc4a92c2edea4099a31c748a5b1f9
phalengsophal-arch <phalengsophal@gmail.com>
2026-05-15 14:18:21 +0700

    Phaleng

--- Algorithm_Phaleng.java
+++ Algorithm_Phaleng.java
@@ -1,3 +1,24 @@
-public class Algorithm_Phaleng {
-    
+public class Algorithm_Phaleng extends Algorithm_V1{
+    @override
+    int sum(int[] input){
+        int sum = 0;
+        for(int i = 0; i < input.length; i++){
+            sum += input[i];
+        }
+        return sum; 
+    }
+
+    @override
+    double average(int[] input){
+
+    }
+
+    public static void main(String[] args) {
+        // 
+        Algorithm_V1 al = new Algorithm_Phaleng();
+
+        System.out.println(Array.toString(al.sample));
+
+        Systen.out.println(al.sum(al.sample));
+    }
 }
