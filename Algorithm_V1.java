4db1cc87e53d76d65e3c1bbae418215b4e855964
Starling <nakboyloy50@gmail.com>
2026-05-15 13:56:21 +0700

    sxa

---
+++ Algorithm_V1.java
@@ -0,0 +1,10 @@
+public class Algorithm_V1 {
+    // attibute
+    int[] sample = {6, 2, 7, 4, 5};
+
+    //abstract sum  (phannag)
+    abstract int sum(int[] input);
+
+    // method avg (paleng)
+    abstract double avg();
+}
