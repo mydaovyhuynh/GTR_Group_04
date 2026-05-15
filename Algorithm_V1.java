0c9f8adb15421a0939f667b9616c38a907b7f753
Starling <nakboyloy50@gmail.com>
2026-05-15 14:42:33 +0700

    update

--- Algorithm_V1.java
+++ Algorithm_V1.java
@@ -2,9 +2,10 @@ public class Algorithm_V1 {
     // attibute
     int[] sample = {6, 2, 7, 4, 5};
 
-    //abstract sum  (phannag)
+    //abstract sum  (phaleng)
     abstract int sum(int[] input);
 
-    // method avg (paleng)
+    // method avg (phannang)
     abstract double average(int[] input);
+
 }
