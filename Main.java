52262bb4f7e56b00b2a94afb48c78550bf729bd4
Starling <nakboyloy50@gmail.com>
2026-05-29 14:20:09 +0700

    update

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +12,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(getClass().getResource("View.fxml"));
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        
         stage.show();
 }
 }
---
+++ controller.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class controller {
+
+    @FXML
+    private Button button_calculate;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    private TextField textField_num1;
+
+    @FXML
+    private TextField textField_num2;
+
+    @FXML
+    void on_calculate(ActionEvent event) {
+
+    }
+
+}
